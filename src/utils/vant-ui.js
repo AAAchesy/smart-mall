aef734fc491a9da8d05331a568aee0a4ec806ca2
AAAchesy <17816304847@163.com>
2026-05-08 02:28:28 +0800

    13.HOME_PAGE

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem } from 'vant'
 
 Vue.use(Button)
 Vue.use(Switch)
@@ -11,3 +11,9 @@ Vue.use(TabbarItem)
 Vue.use(NavBar)
 
 Vue.use(Toast)
+
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
