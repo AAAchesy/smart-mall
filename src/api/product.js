f458a945610d58f1c91b26ddffaded8da49f9889
AAAchesy <17816304847@163.com>
2026-05-27 00:37:53 +0800

    16.PRO_DETAIL_PAGE

--- src/api/product.js
+++ src/api/product.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+// 获取搜索商品列表的数据
 export const getProList = (obj) => {
   const { categoryId, goodsName, page } = obj
   return request.get('/goods/list', {
@@ -7,6 +8,25 @@ export const getProList = (obj) => {
       categoryId,
       goodsName,
       page
+    }
+  })
+}
+
+// 获取商品详情数据
+export const getProDetail = (goodsId) => {
+  return request.get('/goods/detail', {
+    params: {
+      goodsId
+    }
+  })
+}
+
+// 获取商品评价
+export const getProComments = (goodsId, limit) => {
+  return request.get('/comment/listRows', {
+    params: {
+      goodsId,
+      limit
     }
   })
 }
